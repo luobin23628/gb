f0614e047d0850fea5978e15181bbf754f65bc79
binluo <luobin02@baijiahulian.com>
2019-08-01 14:47:16 +0800

    检测未提交代码放到command

--- lib/commands/start.rb
+++ lib/commands/start.rb
@@ -1,8 +1,4 @@
 require 'command'
-require 'yaml'
-require 'config/yml_config'
-require 'colored2'
-require 'gitlab'
 
 module Gitl
   class Start < Command
@@ -62,50 +58,7 @@ module Gitl
 
         end
 
-        changed = g.status.changed
-        added = g.status.added
-        deleted = g.status.deleted
-        untracked = g.status.untracked
-
-        if !changed.empty?
-          alert = true
-          puts "modified files:".red
-          changed.each do |file, status|
-            puts ("        M:        " << file).red
-          end
-        end
-
-        if !added.empty?
-          alert = true
-          puts "added files:".red
-          added.each do |file, status|
-            puts ("        A:        " << file).red
-          end
-        end
-
-        if !deleted.empty?
-          alert = true
-          puts "deleted files:".red
-          deleted.each do |file, status|
-            puts ("        D:        " << file).red
-          end
-        end
-
-        if !untracked.empty?
-          alert = true
-          puts "untracked files:".red
-          untracked.each do |file, status|
-            puts ("        " << file).red
-          end
-        end
-
-        if alert
-          puts "exist uncommit files in current branch '#{g.current_branch}' for '#{project.name}'. ignore it? y/n "
-          flag = STDIN.gets.chomp
-          unless flag.downcase == "y"
-            exit
-          end
-        end
+        check_uncommit(g, project.name)
 
         # 更新本地代码
         g.fetch(remote, :p => true, :t => true)
@@ -151,6 +104,5 @@ module Gitl
 
       end
     end
-
   end
 end
